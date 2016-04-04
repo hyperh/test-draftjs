4954507a1080002f01babb37152af7dfe70dcbd0
HL <nunavutaccountant2@gmail.com>
2016-04-04 22:48:47 +0800

    Fixed no collection

--- server/publications/pubs.js
+++ server/publications/pubs.js
@@ -1,10 +1,9 @@
-import {EditorStates, RawDraftContentStates} from '/lib/collections';
+import {RawDraftContentStates} from '/lib/collections';
 import {Meteor} from 'meteor/meteor';
 
 export default function () {
   Meteor.publish('all', function () {
     return [
-      EditorStates.find({}),
       RawDraftContentStates.find({})
     ];
   });
