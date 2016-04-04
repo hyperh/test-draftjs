307a7b982295e8740dd352961b8258e23f652b1e
HL <nunavutaccountant2@gmail.com>
2016-04-04 17:17:21 +0800

    Setup basic draftjs

--- client/modules/core/components/Home.jsx
+++ client/modules/core/components/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {Editor, EditorState} from 'draft-js';
 
-const Home = () => (
-  <div id="main-page">
-    Hello World
-  </div>
-);
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {editorState: EditorState.createEmpty()};
+    this.onChange = (editorState) => this.setState({editorState});
+  }
+
+  render() {
+    return (
+      <div id="main-page">
+        <h1>Draft.js Editor</h1>
+        <div className="editor">
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+          />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
