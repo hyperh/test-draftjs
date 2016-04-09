6c3172f27682f4549c004831701d4006c17b51a9
HL <nunavutaccountant2@gmail.com>
2016-04-09 15:25:36 +0800

    Setting up drop target

--- client/modules/core/components/Note.jsx
+++ client/modules/core/components/Note.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import { DropTarget } from 'react-dnd';
 
+import Row from './Row.jsx';
 import Widget from './widgets/Widget.jsx';
 
 export default class Note extends Component {
   renderWidgets() {
     const {widgets, removeWidget, noteId} = this.props;
-    return widgets.map(widget => {
+    return widgets.map((widget, index) => {
       return (
-        <Widget
-          key={widget._id}
-          widget={widget}
-          noteId={noteId}
-          removeWidget={removeWidget}
-        />
+        <Row key={widget._id} index={index}>
+          <Widget
+            widget={widget}
+            noteId={noteId}
+            removeWidget={removeWidget}
+          />
+        </Row>
       );
     });
   }
